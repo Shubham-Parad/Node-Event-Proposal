076a0195610855c528f1a21556474d6457e37114
Himanshu Dahare <himanshudahare6400@gmail.com>
2023-06-09 19:59:54 +0530

    routes change

--- router/route.js
+++ router/route.js
@@ -1,17 +1,1 @@
-router.delete("/deleteproposal",async (req,res) => {
-    let {id} = req.body; 
-    try {
-        
-            
-           await proposalModel.findByIdAndDelete(id);
-       const payload =  await proposalModel.find();
-           
-        //    res.send({ status : "ok" });
-           res.send(payload)
 
-          
-   } catch(error)
-   {
-       res.send({ status : "error"});
-   } 
-    });
